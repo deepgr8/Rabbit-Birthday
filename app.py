96d22311d3039ec6fbd465a9e363ac3cb0670e7d
deepgr8 <deepuprajapti5@gmail.com>
2023-03-30 21:56:25 +0530

    new commit

---
+++ app.py
@@ -0,0 +1,15 @@
+
+from flask import render_template,Flask
+import os
+
+os.environ['FLASK_DEBUG'] = 'development'
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "thsdssf"
+
+@app.route('/')
+def index():
+    return render_template('/index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
